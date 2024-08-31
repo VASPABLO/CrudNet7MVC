e35091c17c465f3f35e9bae96bfb7bbd52875d6c
pablo-Vasquez <vaspablo84@gmail.com>
2024-08-31 13:59:40 -0600

    Agregar archivos de proyecto.

---
+++ CrudNet7MVC/Controllers/InicioController.cs
@@ -0,0 +1,151 @@
+﻿using CrudNet7MVC.Datos;
+using CrudNet7MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using System.Diagnostics;
+using System.Net.Mail;
+
+namespace CrudNet7MVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly ApplicationDbContext _contexto;
+
+        public InicioController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+
+        {
+            //Para envio de correo
+            //var mensaje = new MimeMessage();
+            //mensaje.From.Add(new MailboxAddress("Prueba de envio  email", "Vaspablo84@gmail.com"));
+            //mensaje.To.Add(new MailboxAddress("Prueba de enviado  email", "ProgramadorVAsquez @outlook.com"));
+            //mensaje.Subject = "Envio de mensaje de prueba";
+            //mensaje.Body = new TextPart("plain")
+            //{
+            //    Text = "Hola saludos, gracias por registrarte."
+            //};
+
+            //using (var cliente = new SmtpClient())
+            //{
+            //    cliente.Connect("smtp.com", 465);
+            //    cliente.Authenticate("ProgramadorVAsquez @outlook.com")
+            //}
+
+            return View(await _contexto.Contacto.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Contacto.Add(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _contexto.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+                return View();
+            }
+
+            //Borrado
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
